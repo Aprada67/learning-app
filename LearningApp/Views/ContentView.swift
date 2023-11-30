93cf6921b9c680943d198b4d9976fde4c03f4e22
Alberto Prada <albertojprada10@gmail.com>
2023-11-30 10:50:23 +0000

    - Create ContentView, ContentViewRow and HomeViewRow - Create a ScrollView in HomeView - Create a ForEach element in ContentView

---
+++ LearningApp/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  LearningApp
+//
+//  Created by Alberto Jesús Prada Parmera on 29/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearningApp/Views/HomeViewRow.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewRow.swift
+//  LearningApp
+//
+//  Created by Alberto Jesús Prada Parmera on 25/11/2023.
+//
+
+import SwiftUI
+
+struct HomeViewRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeViewRow()
+}
