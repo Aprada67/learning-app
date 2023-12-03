f380eaba10edcd6b847fe3be55d813acdb0a0284
Alberto Prada <albertojprada10@gmail.com>
2023-12-03 14:05:33 +0000

    - Add ContentView - Add constants - Create nextPage func

---
+++ LearningApp/Resources/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  LearningApp
+//
+//  Created by Alberto Jesús Prada Parmera on 30/11/2023.
+//
+
+import Foundation
---
+++ LearningApp/Views/ContentDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentDetailView.swift
+//  LearningApp
+//
+//  Created by Alberto Jesús Prada Parmera on 30/11/2023.
+//
+
+import SwiftUI
+
+struct ContentDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentDetailView()
+}
