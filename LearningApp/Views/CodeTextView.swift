a086e6d8155bde4c0319c5ac496cd62d4e9b7c69
Alberto Prada <albertojprada10@gmail.com>
2023-12-05 13:41:42 +0000

    - Add reusable rectangle card background - Add current contentContenSelected property to the contentModel - Add tag and selection parameters to navigation link to the homeView - Add complete button to the contentDetailView

---
+++ LearningApp/Views/CodeTextView.swift
@@ -0,0 +1,18 @@
+//
+//  CodeTextView.swift
+//  LearningApp
+//
+//  Created by Alberto Jesús Prada Parmera on 03/12/2023.
+//
+
+import SwiftUI
+
+struct CodeTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CodeTextView()
+}
---
+++ LearningApp/Views/RectangleCard.swift
@@ -0,0 +1,18 @@
+//
+//  RectangleCard.swift
+//  LearningApp
+//
+//  Created by Alberto Jesús Prada Parmera on 05/12/2023.
+//
+
+import SwiftUI
+
+struct RectangleCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RectangleCard()
+}
