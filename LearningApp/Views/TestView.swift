6641c90b471e043f1772eac0fcc941b67fa0039d
Alberto Prada <albertojprada10@gmail.com>
2023-12-06 11:19:08 +0000

    - Rename lessonDescription property in ContentModel to codeText - Add testView - Add navigationView in HomeView for test - Add properties in ContentModel to track test selected - Add method to beginTest in ContentModel

---
+++ LearningApp/Views/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  LearningApp
+//
+//  Created by Alberto Jesús Prada Parmera on 06/12/2023.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
