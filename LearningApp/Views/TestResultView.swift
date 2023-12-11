02a8f093749f8372f7cf54d61b266f220582abcd
Alberto Prada <albertojprada10@gmail.com>
2023-12-11 20:56:05 +0000

    - Add the testResultView

--- LearningApp/Views/TestResultView.swift
+++ LearningApp/Views/TestResultView.swift
@@ -8,11 +8,68 @@
 import SwiftUI
 
 struct TestResultView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+    @EnvironmentObject var model: ContentModel
+    
+    var numCorrect: Int
+    
+    var resultHeading: String {
+        
+        // prevent for crashing pct
+        guard model.currentModule != nil else {
+            return ""
+        }
+        
+        let pct = Double(numCorrect)/Double(model.currentModule!.test.questions.count)
+        
+        if pct > 0.5 {
+            return "Awesome"
+        }
+        else if pct > 0.2 {
+            return "Doing great"
+        }
+        else {
+            return "Keep learning"
+        }
     }
-}
-
-#Preview {
-    TestResultView()
+    
+    var body: some View {
+        
+        VStack {
+            
+            Spacer()
+            
+            Text(resultHeading)
+                .font(.title)
+            
+            Spacer()
+            
+            Text("You got \(numCorrect) out of \(model.currentModule?.test.questions.count ?? 0 ) question")
+            
+            Spacer()
+            
+            Button(action: {
+                
+                // Send the user back to the homeView
+                model.currentTestSelected = nil
+                
+            }, label: {
+                
+                ZStack {
+                    
+                    RectangleCard(color: .green)
+                        .frame(height: 48)
+                    
+                    Text("Complete")
+                        .bold()
+                        .foregroundStyle(Color.white)
+                    
+                }
+                
+            })
+            
+            Spacer()
+        }
+        
+    }
 }
