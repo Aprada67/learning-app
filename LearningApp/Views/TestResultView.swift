5742532282a6199598df3cd707806d1c417e2d35
Alberto Prada <albertojprada10@gmail.com>
2023-12-11 20:54:00 +0000

    - Add the testResultView - Show the testResultView in the testView after the last question

---
+++ LearningApp/Views/TestResultView.swift
@@ -0,0 +1,18 @@
+//
+//  TestResultView.swift
+//  LearningApp
+//
+//  Created by Alberto Jesús Prada Parmera on 11/12/2023.
+//
+
+import SwiftUI
+
+struct TestResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestResultView()
+}
